72353dba01cd4943912be5202af02d0ce2bbf7e4
Linus Boréus <linusboreus@outlook.com>
2021-09-12 21:57:44 +0200

    Ground work for mqtt and file saving

--- app.ts
+++ app.ts
@@ -1,23 +1,14 @@
 import path = require("path");
+import mqtt = require("mqtt");
 import Color = require("color");
+import mdns = require("mdns")
 import express = require("express");
 import date = require("date-and-time");
 import compression = require("compression");
-
-interface Settings {
-  name: string;
-  color: Color;
-  order: number;
-}
-
-interface Reading {
-  id: string;
-  temperature: number;
-  time: Date;
-}
+import { readSavedData, TempLog, Settings, Reading } from "./fileManagement";
 
 interface Data {
-  x: string;
+  x: Date;
   y: number;
 }
 
@@ -34,14 +25,7 @@ interface ChartData {
   options: any;
 }
 
-interface TempLog {
-  [index: string]: {
-    settings: Settings;
-    data?: [];
-  };
-}
-
-const root = path.join(__dirname, "../");
+const rootFolder = path.join(__dirname, "../");
 
 const chartOptions = {
   responsive: true,
@@ -57,11 +41,15 @@ const chartOptions = {
 };
 
 const port = 3000;
-
 const app = express();
 const router = express.Router();
+const client = mqtt.connect("mqtt://127.0.0.1:1883");
 
 let temperatureLog: TempLog;
+
+readSavedData().then(val => temperatureLog = val);
+mdns.createAdvertisement(mdns.tcp("tempbroker"), 1883, { name: "TemperatureBroker" });
+
 
 // setup web hosting
 app.use("/", router);
@@ -70,7 +58,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
-app.set("views", path.join(root, "views"));
+app.set("views", path.join(rootFolder, "views"));
 
 // reports the last temperature reading of every sensor
 function currentTemp(): Reading[] {
@@ -113,20 +101,20 @@ router.post("/settings", (req, res) => {
 });
 
 router.get("/log", (req, res) => {
-  let currentTemp = currentTemp();
+  let currentTemp; // = currentTemp();
   const readings = []; // fix later
   res.render("log", {
-    currentTemp: currentTemp,
+    currentTemp: currentTemp(),
     readings: readings,
     time: date.format(new Date(), "[Last update: ]HH:mm:ss"),
   });
 });
 
 router.get("/about", (req, res) => {
-  let currentTemp = currentTemp();
+  let currentTemp; // = currentTemp();
 
   res.render("about", {
-    currentTemp,
+    currentTemp: currentTemp,
     time: date.format(new Date(), "[Last update: ]HH:mm:ss"),
   });
 });
@@ -137,11 +125,49 @@ router.get("/data", async (req, res) => {
     datasets: [],
     options: chartOptions,
   };
+  if (temperatureLog) {
+    Object.keys(temperatureLog).forEach(id => {
+      chartData.datasets[temperatureLog[id].settings.order] = {
+        label: temperatureLog[id].settings.name,
+        data: temperatureLog[id].data.map(reading => { return { x: reading.time, y: reading.temperature } }),
+        borderColor: Color,
+        backgroundColor: Color,
+      };
+    });
+  }
   res.json(chartData);
 });
 
 router.get("/chart.min.js", async (req, res) => {
-  res.sendFile(path.join(root, "node_modules/chart.js/dist/chart.min.js"));
+  res.sendFile(path.join(rootFolder, "node_modules/chart.js/dist/chart.min.js"));
 });
+
+client.on("connect", _ => {
+  console.log("Connected to mqtt broker!");
+  client.subscribe("temperature/+/reading", () => {
+    console.log("Subscribed to: temperature/+/reading\n+ = the id of the sensor");
+
+  });
+});
+
+client.on("message", (topic, message) => {
+  const topicHirarcy = topic.split("/");
+  const id = topicHirarcy[1];
+
+  // expected topic is "temperature/{id}/reading"
+  if (topicHirarcy[0] === "temperature") {
+    if (topicHirarcy[2] === "reading")
+      // should probably do some error checking on message
+      // expected message: {temperature: ##}
+      temperatureLog[id].data.push({ temperature: JSON.parse(message.toString()).temperature, time: new Date() });
+  }
+  else {
+    console.log("Received topic:" + topic + "\nWhich is not expected!");
+  }
+
+});
+
+client.on("offline", _ => console.log("Broker offline!"));
+
 
 app.listen(process.env.port || port);
--- fileManagement.ts
+++ fileManagement.ts
@@ -2,84 +2,136 @@ import fs = require("fs");
 import path = require("path");
 import Color = require("color");
 
-interface Settings {
-  name: string;
-  color: Color;
-  order: number;
+export interface Settings {
+  name: string,
+  color: Color,
+  order: number,
 }
 
-interface TempLog {
-  [index: string]: {
+export interface Reading {
+  temperature: number;
+  time: Date;
+}
+
+export interface TempLog {
+  [id: string]: {
     settings: Settings;
-    data?: [];
+    data?: [Reading];
   };
 }
-const root = path.join(__dirname, "../");
-const dataFolder = root + "data";
+
+interface IdFileHandle {
+  [id: string]: fs.WriteStream;
+}
+
+const rootFolder = path.join(__dirname, "../");
+const dataFolder = rootFolder + "data";
+let logFiles: IdFileHandle;
 
 async function readContents(folder: string): Promise<string[]> {
   if (!fs.existsSync(folder)) {
-    fs.mkdir(folder, (err) => console.log(err));
+    fs.mkdir(folder, (err) => error(err));
   } else {
     let contents: string[];
     fs.readdir(folder, (err, files) => {
-      if (err) console.log(err);
+      if (err) error(err);
       else contents = files;
     });
-    console.log(contents);
     return contents;
   }
-  return [];
+  throw new Error("Creating folder: " + folder);
 }
 
 function error(err: NodeJS.ErrnoException) {
-  if (err) {
-    if (err.code === "ENOENT") {
-      console.error(err.path + "does not exist");
-      return;
+  if (err.code === "ENOENT") {
+    console.error(err.path + "does not exist");
+    return;
+  }
+  console.log(err);
+}
+
+function getPath(id) {
+  const propertiesPath = `${dataFolder}/${id}/properties.json`;
+  const logPath = `${dataFolder}/${id}/readings.log`;
+  return { logPath, propertiesPath };
+}
+
+async function readFiles(id: string): Promise<TempLog> {
+  const { logPath, propertiesPath } = getPath(id);
+
+  const settings = new Promise<TempLog>((resolve) => fs.open(propertiesPath, "r", (err, fd) => {
+    error(err);
+    try {
+      fs.readFile(fd, { encoding: "utf8" }, (err, data) => {
+        error(err);
+        resolve({ id: { settings: JSON.parse(data) } });
+      });
+    } finally {
+      fs.close(fd, error);
     }
-    throw err;
+  }));
+
+  const readings = new Promise<TempLog>((resolve, reject) => fs.open(logPath, "r", (err, fd) => {
+    error(err);
+    try {
+      fs.readFile(fd, { encoding: "utf8" }, (err, data) => {
+        error(err);
+        resolve({ id: { settings: null, data: JSON.parse("[" + data.slice(0, -2) + "]") } });
+      });
+    } finally {
+      fs.close(fd, (err) => { if (err) { error(err); reject; } });
+    }
+  }));
+
+  return Promise.all([settings, readings]).then((val) => {
+    let tempLog: TempLog;
+    val.forEach(element => {
+      if (element.id.data) tempLog.id.data = element.id.data;
+      if (element.id.settings) tempLog.id.settings = element.id.settings;
+    });
+    return tempLog;
+  });
+}
+
+// reads all of the saved data
+export async function readSavedData(): Promise<TempLog> {
+  const temperatureLog = readContents(dataFolder)
+    .then(contents => {
+      if (contents) {
+        return contents.filter(contents => fs.statSync(dataFolder + "/" + contents).isDirectory());
+      }
+    })
+    .then(idFolders => {
+      let tempLog: TempLog;
+      if (idFolders) {
+        idFolders.forEach(id => { readFiles(id).then(val => { if (val.id) tempLog[id] = val.id; }) });
+      } else throw new Error("No saved sensor data found");
+      return tempLog;
+    })
+    .catch(err => {
+      error(err)
+      let tempLog: TempLog;
+      return tempLog;
+    });
+
+  return temperatureLog;
+}
+
+export async function saveReading(id: string, reading: Reading): Promise<void> {
+  await readContents(dataFolder + "/" + id);
+  const { logPath } = getPath(id);
+  if (logFiles[id]) {
+    logFiles[id].write(JSON.stringify(reading) + ",\n");
+  } else {
+    logFiles[id] = fs.createWriteStream(logPath, { flags: "a" });
+    logFiles[id].write(JSON.stringify(reading) + ",\n");
   }
 }
 
-function readFiles(id: string) {
-  let temperatureLog: TempLog;
-  const propertiesPath = `${dataFolder}/${id}/properties.json`;
-  const logPath = `${dataFolder}/${id}/readings.log`;
-
-  fs.open(propertiesPath, "r", (err, fd) => {
-    error(err);
-    try {
-      fs.readFile(fd, { encoding: "utf8" }, (err, data) => {
-        error(err);
-        temperatureLog.id.settings = JSON.parse(data);
-      });
-    } finally {
-      fs.close(fd, error);
-    }
-  });
-
-  fs.open(logPath, "r", (err, fd) => {
-    error(err);
-    try {
-      fs.readFile(fd, { encoding: "utf8" }, (err, data) => {
-        error(err);
-        temperatureLog.id.data = JSON.parse("[" + data.slice(0, -2) + "]");
-      });
-    } finally {
-      fs.close(fd, error);
-    }
-  });
-}
-
-//
-async function readSavedData() {
-  const dataContents = readContents(dataFolder);
-  //let settingsContents = readContents(settingsFolder);
-
-  const idFolders = (await dataContents).filter((contents) => {
-    return fs.statSync(dataFolder + "/" + contents).isDirectory();
-  });
-
-  idFolders.forEach(readFiles);
+export function saveSettings(id: string, settings: Settings): void {
+  readContents(dataFolder + "/" + id);
+  const { propertiesPath } = getPath(id);
+  const settingsFile = fs.createWriteStream(propertiesPath, { flags: "w" });
+  settingsFile.write(JSON.stringify(settings));
+  settingsFile.close();
 }
